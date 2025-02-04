0a6e75bfde7e03e47925de25fccca9877cd11c52
ydbf <dan.mcneill2002@gmail.com>
2025-02-05 09:49:59 +1100

    Update getEpgs.py

--- epg-grabber/getEpgs.py
+++ epg-grabber/getEpgs.py
@@ -68,9 +68,16 @@ def filter_and_build_epg(urls):
         print(f"New EPG saved to {output_file_gz}")
 	    
 m3u4u_epg = os.getenv("M3U4U_EPG")
-
 urls = [
-        'https://epgshare01.online/epgshare01/epg_ripper_ALL_SOURCES1.xml.gz',
+        'https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz',
+        'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz',
+	'https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz',
+	'https://epgshare01.online/epgshare01/epg_ripper_UK1.xml.gz',
+	'https://epgshare01.online/epgshare01/epg_ripper_AU1.xml.gz',
+	'https://epgshare01.online/epgshare01/epg_ripper_IE1.xml.gz',
+	'https://epgshare01.online/epgshare01/epg_ripper_DE1.xml.gz',
+	'https://epgshare01.online/epgshare01/epg_ripper_ZA1.xml.gz',
+	'https://epgshare01.online/epgshare01/epg_ripper_DUMMY_CHANNELS.xml.gz',
 ]
 
 if __name__ == "__main__":
